bce2e84148fa366fd86392afbac67bb77e925fd7
rkun <tren111213@gmail.com>
2016-09-20 14:01:18 +0900

    first commit v0.1git add .

---
+++ LoopDownloader.py
@@ -0,0 +1,38 @@
+import requests
+import os
+import re
+import sys
+from PIL import Image
+from io import StringIO
+
+print("Hi")
+
+count = 1
+
+pat = r"num"
+urlData = sys.argv[1]
+
+kakuchoshi = sys.argv[2]
+loop = True
+
+if os.path.isdir("data") != True:
+    os.mkdir("data")
+else:
+    print("directory has already existed.")
+
+while loop:
+    url = re.sub(pat,str(count),urlData)
+    data = requests.get(url)
+
+    if data.status_code == 200:
+        print (str(count)+"th image is Downloading...")
+        fn = "data/data"+str(count)+"."+kakuchoshi
+        #print(fn)
+
+        f = open(fn,'wb+')
+        f.write(data.content)
+        count+=1
+
+    else:
+        loop = False
+print("All finished!!")
---
+++ readme.md
@@ -0,0 +1,9 @@
+# LoopDownloader
+## What is this?
+これは、URLに連番が含まれているファイルを順番に自動でダウンロードできるPythonスクリプトです。
+## Usage
+`python LoopDownloader.py {連番部分を「num」に置き換えたurl} {ファイルの拡張子}`
+
+## Maker
+### rkun
+- [@rkun123](twitter.com/rkun123)
